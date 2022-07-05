cd8b13a6f44ad89e915b9286d91aa0cf634e1b93
marcoaureliotomaz <marcoaureliotomaz@gmail.com>
2022-07-04 22:41:37 -0300

    Exclusão de pessoa por código com tratamento de erro caso o registro não exista

--- src/main/java/com/example/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/example/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -8,6 +8,7 @@ import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -44,6 +47,16 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		List<Erro> erros = criarListaDeErros(ex.getBindingResult());
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request) ;
 	}
+	
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	//@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<Object> handleEmptyResultDataAccesExcpetion(EmptyResultDataAccessException ex, WebRequest request) {
+		String messagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null,LocaleContextHolder.getLocale());
+		String messageDesenvolvedor = ex.toString();
+		List<Erro> erros = Arrays.asList(new Erro(messagemUsuario, messageDesenvolvedor));		
+		return handleExceptionInternal(ex, erros,   new HttpHeaders(),  HttpStatus.NOT_FOUND, request);
+				}
+	
 	
 	private List<Erro> criarListaDeErros(BindingResult bindingResult){
 		List<Erro> erros = new ArrayList<>();
--- src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,4 +52,11 @@ public class PessoaResource {
 		Pessoa pessoaEncontrada = pessoaRepository.findById(codigo).orElse(null);
 		return pessoaEncontrada != null ? ResponseEntity.ok(pessoaEncontrada) : ResponseEntity.notFound().build();
 	}
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pessoaRepository.deleteById(codigo);
+		
+	}
+	
 }
